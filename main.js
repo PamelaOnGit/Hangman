a4642723509709ea44f496bdcfe4ae387d527242
PamelaOnGit <pammyokeeffe@gmail.com>
2022-06-19 13:19:11 +0100

    modifications to code

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const wrongLetters = document.getElementById('wrongletters');
 const showLives = document.getElementById('livescontainer'); 
 const numLives = document.getElementById('lives'); 
 const winLoseContainer = document.getElementById('winlosecontainer'); 
+const winLoseMessage = document.getElementById('winlosemessage'); 
 const instructions = document.getElementById('instructions'); 
 const alphabet = document.getElementById('alphabet'); 
 const livesContainer = document.getElementById('livescontainer'); 
+const playAgain = document.getElementById('playagain'); 
+
 
 
 
@@ -22,365 +25,268 @@ const oButton = document.getElementById('o');
 const tButton = document.getElementById('t');
 const uButton = document.getElementById('u');
 const wButton = document.getElementById('w');
+const yButton = document.getElementById('y'); 
+const zButton = document.getElementById('z'); 
 
 
-const wordSelection = ['cat', 'bat', 'dog', 'cow', 'duck', 'goat', 'toad']
+
+const wordSelection = ['cat', 'bat', 'dog', 'cow', 'duck', 'goat', 'toad', 'bee', 'gecko']
+//const wordSelection = ['zoo', 'ozz', 'eze']; 
+//const wordSelection = ['cow']; 
 let randomNum = Math.floor(Math.random() * wordSelection.length); 
 let word = wordSelection[randomNum]; 
 let wordArray = Array.from(word); 
 let lives = 5; 
 
 
-function play() { 
-        for (i=0; i<wordArray.length; i++){
-        let hiddenLetter = document.createElement('span'); 
-        hiddenLetter.setAttribute('id', `sp${i}`);
-        hiddenLetter.innerHTML = '?'; 
-        hiddenWord.appendChild(hiddenLetter); 
-    } 
-    playBtn.style.display = 'none';
-    showLives.style.display = 'block';  
-}; 
 
-playBtn.addEventListener('click', play); 
+
+
+
+function resetGame(){ 
+    for (i=0;i<word.length; i++){ 
+        let eachLetter = document.getElementById(`sp${i}`); 
+        hiddenWord.removeChild(eachLetter); 
+    }; 
+
+    instructions.innerHTML = 'Choose a letter and guess the animal!';
+
+    lives = 5; 
+    numLives.innerHTML = lives; 
+    livesContainer.style.display = 'block'; 
+
+    winLoseContainer.style.display = 'none'; 
+    
+    instructions.style.display = 'block'; 
+    hiddenWord.style.display = 'inline'; 
+    alphabet.style.display = 'block'; 
+
+    
+    aButton.style.display = 'inline'; 
+    aButton.style.backgroundColor = 'beige'; 
+    bButton.style.display = 'inline';
+    bButton.style.backgroundColor = 'beige';
+    cButton.style.display = 'inline'; 
+    cButton.style.backgroundColor = 'beige'; 
+    dButton.style.display = 'inline'; 
+    dButton.style.backgroundColor = 'beige'; 
+    eButton.style.display = 'inline'; 
+    eButton.style.backgroundColor = 'beige'; 
+    fButton.style.display = 'inline'; 
+    fButton.style.backgroundColor = 'beige'; 
+    gButton.style.display = 'inline'; 
+    gButton.style.backgroundColor = 'beige'; 
+    kButton.style.display = 'inline'; 
+    kButton.style.backgroundColor = 'beige'; 
+    oButton.style.display = 'inline'; 
+    oButton.style.backgroundColor = 'beige'; 
+    tButton.style.display = 'inline';
+    tButton.style.backgroundColor = 'beige';
+    uButton.style.display = 'inline';
+    uButton.style.backgroundColor = 'beige'; 
+    wButton.style.display = 'inline'; 
+    wButton.style.backgroundColor = 'beige'; 
+    yButton.style.display = 'inline'; 
+    yButton.style.backgroundColor = 'beige'; 
+    zButton.style.display = 'inline'; 
+    zButton.style.backgroundColor = 'beige'; 
+
+    wrongLetters.style.display = 'block'; 
+
+    randomNum = Math.floor(Math.random() * wordSelection.length); 
+    word = wordSelection[randomNum]; 
+    wordArray = Array.from(word); 
+
+    play(); 
+    
+}
+playAgain.addEventListener('click', resetGame); 
+
+function win(){ 
+    let winArray = []; 
+    for (i=0; i<word.length; i++){
+    let winLetter = document.getElementById(`sp${i}`).innerHTML;   
+    winArray.push(winLetter);   
+    }
+    console.log(winArray); 
+    console.log(wordArray); 
+    if (winArray.join() === wordArray.join()){ 
+       instructions.style.display = 'none'; 
+       hiddenWord.style.display = 'none'; 
+       alphabet.style.display = 'none'; 
+       livesContainer.style.display = 'none'; 
+       //wrongLetters.style.display = 'none'; 
+       winLoseMessage.innerHTML = `That's right - ${word} - You got it!`
+       winLoseContainer.style.display = 'block';
+    
+    }
+    playAgain.addEventListener('click', function(){
+        console.log('playAgain function works');
+    })
+}
+
+function incorrectLetter(alph){ 
+    if (!word.includes(alph)){ 
+        let alphButton = document.getElementById(alph); 
+        alphButton.style.backgroundColor = 'pink'; 
+        //alphButton.removeEventListener('click', zPress);
+        lives = lives - 1; 
+        if (lives > 0){ 
+            numLives.innerHTML = lives; 
+        }else{
+            instructions.style.display = 'none'; 
+            alphabet.style.display = 'none'; 
+            livesContainer.style.display = 'none'; 
+            wrongLetters.style.display = 'none'; 
+            winLoseMessage.innerHTML = 'You lose this time - try again!'
+            winLoseContainer.style.display = 'block'; 
+        }
+    }
+}
+
+function correctLetter(alph) { 
+    if (word.includes(alph)){
+        for (i=0; i<word.length; i++){ 
+            if (wordArray[i] === alph){
+                let alphCorrect = document.getElementById(`sp${i}`); 
+                alphCorrect.innerHTML = alph; 
+            }
+        }
+    }
+}
 
 function aPress(){ 
-    aButton.style.display = 'none';  
-    if(!word.includes('a')){ 
-        lives = lives - 1;  
-        let aWrong = document.createElement('span'); 
-        aWrong.innerHTML = 'a'; 
-        wrongLetters.appendChild(aWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('a')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'a'){
-            let aCorrect = document.getElementById(`sp${i}`); 
-            aCorrect.innerHTML = 'a';             
-            }
-        }   
-    }    
-};  
-aButton.addEventListener('click', aPress) 
+    correctLetter('a'); 
+    incorrectLetter('a'); 
+    win(); 
+}
 
-function bPress(){  
-    bButton.style.display = 'none'; 
-    if(!word.includes('b')){ 
-        lives = lives - 1;  
-        let bWrong = document.createElement('span'); 
-        bWrong.innerHTML = 'b'; 
-        wrongLetters.appendChild(bWrong); 
-    } 
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none';
-        winLoseContainer.style.display = 'block'; 
-    }
-    for(i=0; i<wordArray.length; i++){
-        if (wordArray[i] === 'b'){
-            let bCorrect = document.getElementById(`sp${i}`); 
-            bCorrect.innerHTML = 'b';             
-        }
-    } 
-     
-};  
-bButton.addEventListener('click', bPress) 
 
-function cPress(){
-    cButton.style.display = 'none'; 
-    if(!word.includes('c')){ 
-        lives = lives -1; 
-        let cWrong = document.createElement('span'); 
-        cWrong.innerHTML = 'c'; 
-        wrongLetters.appendChild(cWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    for(i=0; i<wordArray.length; i++){ 
-        if (wordArray[i] === 'c'){
-            let cCorrect = document.getElementById(`sp${i}`); 
-            cCorrect.innerHTML = 'c'; 
-        }
-    }
-    numLives.innerHTML = lives; 
-}; 
-cButton.addEventListener('click', cPress); 
+function bPress(){ 
+    correctLetter('b'); 
+    incorrectLetter('b'); 
+    win(); 
+}
 
-function dPress(){  
-    dButton.style.display = 'none'; 
-    if(!word.includes('d')){ 
-        lives = lives - 1;  
-        let dWrong = document.createElement('span'); 
-        dWrong.innerHTML = 'd'; 
-        wrongLetters.appendChild(dWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    for(i=0; i<wordArray.length; i++){
-        if (wordArray[i] === 'd'){
-            let dCorrect = document.getElementById(`sp${i}`); 
-            dCorrect.innerHTML = 'd';             
-        }
-    } 
-    numLives.innerHTML = lives; 
-};  
-dButton.addEventListener('click', dPress) 
+
+function cPress() {
+    correctLetter('c'); 
+    incorrectLetter('c'); 
+    win(); 
+}
+
+
+function dPress(){ 
+    correctLetter('d'); 
+    incorrectLetter('d'); 
+    win(); 
+}
+
 
 function ePress(){ 
-    eButton.style.display = 'none';  
-    if(!word.includes('e')){ 
-        lives = lives - 1;  
-        let eWrong = document.createElement('span'); 
-        eWrong.innerHTML = 'e'; 
-        wrongLetters.appendChild(eWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('e')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'e'){
-            let eCorrect = document.getElementById(`sp${i}`); 
-            eCorrect.innerHTML = 'e';             
-            }
-        }   
-    }    
-};  
-eButton.addEventListener('click', ePress) 
+    correctLetter('e'); 
+    incorrectLetter('e'); 
+    win(); 
+}
 
-function fPress(){ 
-    fButton.style.display = 'none';  
-    if(!word.includes('f')){ 
-        lives = lives - 1;  
-        let fWrong = document.createElement('span'); 
-        fWrong.innerHTML = 'f'; 
-        wrongLetters.appendChild(fWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('f')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'f'){
-            let fCorrect = document.getElementById(`sp${i}`); 
-            fCorrect.innerHTML = 'f';             
-            }
-        }   
-    }    
-};  
-fButton.addEventListener('click', fPress) 
 
-function gPress(){ 
-    gButton.style.display = 'none';  
-    if(!word.includes('g')){ 
-        lives = lives - 1;  
-        let gWrong = document.createElement('span'); 
-        gWrong.innerHTML = 'g'; 
-        wrongLetters.appendChild(gWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('g')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'g'){
-            let gCorrect = document.getElementById(`sp${i}`); 
-            gCorrect.innerHTML = 'g';             
-            }
-        }   
-    }    
-};  
-gButton.addEventListener('click', gPress) 
+function fPress(){
+    correctLetter('f'); 
+    incorrectLetter('f'); 
+    win(); 
+}
+
+
+function gPress(){
+    correctLetter('g'); 
+    incorrectLetter('g'); 
+    win(); 
+}
 
 function kPress(){ 
-    kButton.style.display = 'none';  
-    if(!word.includes('k')){ 
-        lives = lives - 1;  
-        let kWrong = document.createElement('span'); 
-        kWrong.innerHTML = 'k'; 
-        wrongLetters.appendChild(kWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('k')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'k'){
-            let kCorrect = document.getElementById(`sp${i}`); 
-            kCorrect.innerHTML = 'k';             
-            }
-        }   
-    }    
-};  
-kButton.addEventListener('click', kPress) 
+    correctLetter('k'); 
+    incorrectLetter('k'); 
+    win(); 
+}
 
-function tPress(){ 
-    tButton.style.display = 'none';  
-    if(!word.includes('t')){ 
-        lives = lives - 1;  
-        let tWrong = document.createElement('span'); 
-        tWrong.innerHTML = 't'; 
-        wrongLetters.appendChild(tWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('t')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 't'){
-            let tCorrect = document.getElementById(`sp${i}`); 
-            tCorrect.innerHTML = 't';             
-            }
-        }   
-    }    
-};  
-tButton.addEventListener('click', tPress) 
 
 function oPress(){ 
-    oButton.style.display = 'none';  
-    if(!word.includes('o')){ 
-        lives = lives - 1;  
-        let oWrong = document.createElement('span'); 
-        oWrong.innerHTML = 'o'; 
-        wrongLetters.appendChild(oWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('o')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'o'){
-            let oCorrect = document.getElementById(`sp${i}`); 
-            oCorrect.innerHTML = 'o';             
-            }
-        }   
-    }    
-};  
-oButton.addEventListener('click', oPress) 
+    correctLetter('o'); 
+    incorrectLetter('o'); 
+    win(); 
+}
 
 
-
-
+function tPress(){ 
+    correctLetter('t'); 
+    incorrectLetter('t'); 
+    win(); 
+}
 
 
 function uPress(){ 
-    uButton.style.display = 'none';  
-    if(!word.includes('u')){ 
-        lives = lives - 1;  
-        let uWrong = document.createElement('span'); 
-        uWrong.innerHTML = 'u'; 
-        wrongLetters.appendChild(uWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('u')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'u'){
-            let uCorrect = document.getElementById(`sp${i}`); 
-            uCorrect.innerHTML = 'u';             
-            }
-        }   
-    }    
-};  
-uButton.addEventListener('click', uPress) 
+    correctLetter('u'); 
+    incorrectLetter('u'); 
+    win(); 
+}
+
 
 function wPress(){ 
-    wButton.style.display = 'none';  
-    if(!word.includes('w')){ 
-        lives = lives - 1;  
-        let wWrong = document.createElement('span'); 
-        wWrong.innerHTML = 'w'; 
-        wrongLetters.appendChild(wWrong); 
-    }
-    if (lives>0){
-        numLives.innerHTML = lives; 
-    }else{
-        instructions.style.display = 'none'; 
-        alphabet.style.display = 'none'; 
-        livesContainer.style.display = 'none'; 
-        wrongLetters.style.display = 'none'; 
-        winLoseContainer.style.display = 'block'; 
-    }
-    if(word.includes('w')){
-        for(i=0; i<wordArray.length; i++){
-            if (wordArray[i] === 'w'){
-            let wCorrect = document.getElementById(`sp${i}`); 
-            wCorrect.innerHTML = 'w';             
-            }
-        }   
-    }    
-};  
-wButton.addEventListener('click', wPress) 
+    correctLetter('w');
+    incorrectLetter('w'); 
+    win(); 
+}
+
+
+function yPress() { 
+    correctLetter('y'); 
+    incorrectLetter('y'); 
+    win(); 
+}
+
+
+function zPress() { 
+    correctLetter('z'); 
+    incorrectLetter('z'); 
+    win(); 
+}
+
+function play() { 
+
+    for (i=0; i<wordArray.length; i++){
+    let hiddenLetter = document.createElement('span'); 
+    hiddenLetter.setAttribute('id', `sp${i}`);
+    hiddenLetter.innerHTML = '?'; 
+    hiddenWord.appendChild(hiddenLetter); 
+} 
+
+aButton.addEventListener('click', aPress, {once: true}); 
+bButton.addEventListener('click', bPress, {once: true}); 
+cButton.addEventListener('click', cPress, {once: true}); 
+dButton.addEventListener('click', dPress, {once: true}); 
+eButton.addEventListener('click', ePress, {once: true}); 
+fButton.addEventListener('click', fPress, {once: true}); 
+gButton.addEventListener('click', gPress, {once: true}); 
+kButton.addEventListener('click', kPress, {once: true}); 
+oButton.addEventListener('click', oPress, {once: true}); 
+tButton.addEventListener('click', tPress, {once: true}); 
+uButton.addEventListener('click', uPress, {once: true}); 
+wButton.addEventListener('click', wPress, {once: true}); 
+yButton.addEventListener('click', yPress, {once: true}); 
+zButton.addEventListener('click', zPress, {once: true}); 
+
+
+playBtn.style.display = 'none';
+instructions.style.display = 'none'; 
+showLives.style.display = 'block';  
+}; 
+playBtn.addEventListener('click', play); 
+
+
+
+
+
+
 
 
 
